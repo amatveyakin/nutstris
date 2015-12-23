6aebd3d18bf6dd83d6067187c5c8b8b3b2dfaff9
Alexey Eremin <a.eremin.msu@gmail.com>
2015-12-23 14:31:19 +0100

    Fix formatting in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,36 @@
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
 
-int main()
-{
-    sf::Window window(sf::VideoMode(800, 600), "OpenGL");
-    window.setVerticalSyncEnabled(true);
+int main() {
+  sf::Window window(sf::VideoMode(800, 600), "OpenGL");
+  window.setVerticalSyncEnabled(true);
 
-    // load resources, initialize the OpenGL states, ...
+  // load resources, initialize the OpenGL states, ...
 
-    // run the main loop
-    bool running = true;
-    while (running)
-    {
-        // handle events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                // end the program
-                running = false;
-            }
-            else if (event.type == sf::Event::Resized)
-            {
-                // adjust the viewport when the window is resized
-                glViewport(0, 0, event.size.width, event.size.height);
-            }
-        }
-
-        // clear the buffers
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // draw...
-
-        // end the current frame (internally swaps the front and back buffers)
-        window.display();
+  // run the main loop
+  bool running = true;
+  while (running) {
+    // handle events
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        // end the program
+        running = false;
+      }
+      else if (event.type == sf::Event::Resized) {
+        // adjust the viewport when the window is resized
+        glViewport(0, 0, event.size.width, event.size.height);
+      }
     }
 
-    return 0;
+    // clear the buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // draw...
+
+    // end the current frame (internally swaps the front and back buffers)
+    window.display();
+  }
+
+  return 0;
 }

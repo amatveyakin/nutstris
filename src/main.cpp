edf32cbed808c68de937b6935d2acc702cf1b284
Alexey Eremin <a.eremin.msu@gmail.com>
2015-12-23 01:25:54 +0100

    Add OpenGL initialization according to SFML tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,41 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+    sf::Window window(sf::VideoMode(800, 600), "OpenGL");
+    window.setVerticalSyncEnabled(true);
 
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed)
-        window.close();
+    // load resources, initialize the OpenGL states, ...
+
+    // run the main loop
+    bool running = true;
+    while (running)
+    {
+        // handle events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                // end the program
+                running = false;
+            }
+            else if (event.type == sf::Event::Resized)
+            {
+                // adjust the viewport when the window is resized
+                glViewport(0, 0, event.size.width, event.size.height);
+            }
+        }
+
+        // clear the buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw...
+
+        // end the current frame (internally swaps the front and back buffers)
+        window.display();
     }
 
-    window.clear();
-    window.draw(shape);
-    window.display();
-  }
-
-  return 0;
+    return 0;
 }

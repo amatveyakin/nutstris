51a6ea414c8c53550f5288f3f476e3a693d7ff22
Alexey Eremin <a.eremin.msu@gmail.com>
2015-12-26 02:26:30 +0100

    Fix some tabs

--- src/render/renderer.cpp
+++ src/render/renderer.cpp
@@ -19,8 +19,8 @@ void Renderer::render() {
   for (int i = -5; i <= 5; ++i)
     for (int j = -5; j <= 5; ++j)
       cubesData.push_back({
-	math::Mat4x4f::translationMatrix({i * 1.0f, j * 1.0f, 0.0f}),
-	math::Vec3f(0.5 + i * 0.1f, 0.5 + j * 0.1f, 0.0f)
+        math::Mat4x4f::translationMatrix({i * 1.0f, j * 1.0f, 0.0f}),
+        math::Vec3f(0.5 + i * 0.1f, 0.5 + j * 0.1f, 0.0f)
       });
   cubeMesh_->render(cubesData);
 }

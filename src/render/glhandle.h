b26713d94d0c5481d37ea5c5d8da9d59c68e6ff1
Alexey Eremin <a.eremin.msu@gmail.com>
2016-01-10 13:52:53 +0300

    Fix: use GL_INVALID_INDEX instead of zero in GLHandle move ctor

--- src/render/glhandle.h
+++ src/render/glhandle.h
@@ -8,7 +8,7 @@ class GLHandle
 {
 public:
     GLHandle(GLuint id) : id_(id) {}
-    GLHandle(GLHandle&& other) : id_(other.id_) { other.id_ = 0; }
+    GLHandle(GLHandle&& other) : id_(other.id_) { other.id_ = GL_INVALID_INDEX; }
     GLHandle(const GLHandle& other) = delete;
     const GLHandle& operator=(const GLHandle& other) = delete;
 

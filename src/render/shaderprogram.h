1563287ed94add0a6643174bced4a91c7a8d89bc
Alexey Eremin <a.eremin.msu@gmail.com>
2016-01-10 16:55:30 +0300

    Add ShaderProgram::setUniformBuffer

--- src/render/shaderprogram.h
+++ src/render/shaderprogram.h
@@ -6,6 +6,7 @@
 #include "math/matrix.h"
 #include "math/vector.h"
 #include "render/glhandle.h"
+#include "render/uniformbuffer.h"
 
 
 namespace render {
@@ -16,6 +17,12 @@ class ShaderProgram {
 public:
   ShaderProgram(VertexShader& vertexShader, FragmentShader& fragmentShader);
   ~ShaderProgram();
+
+  template<typename T>
+  void ShaderProgram::setUniformBuffer(const std::string& name, const UniformBuffer<T>& buffer) {
+    GLuint blockIndex = glGetUniformBlockIndex(id_, name.c_str());
+    buffer.bindTo(blockIndex);
+  }
 
   void setUniform(const std::string& name, const math::Mat4x4f& value);
   void setUniform(const std::string& name, const math::Vec4f& value);

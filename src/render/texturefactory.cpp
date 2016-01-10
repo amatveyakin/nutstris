cbcee7c6f8edc0ba1c28b86ddd90dbdf75a7a325
Andrei Matveiakin <a.matveyakin@gmail.com>
2016-01-10 20:18:26 +0100

    Reuse bonus names constants

--- src/render/texturefactory.cpp
+++ src/render/texturefactory.cpp
@@ -1,32 +1,20 @@
 #include "texturefactory.h"
+
 #include <map>
 #include <string>
 #include <cassert>
+
 #include <GL/glew.h>
 #include <SFML/Graphics/Image.hpp>
+
+#include "engine/bonus.h"
+#include "engine/strings.h"
 
 namespace render {
 
 namespace
 {
 const std::string kTextureBonusesPath = "resources/textures/bonuses/";
-const std::map<int, std::string> kTextureBonusesNames = {
-  {0, "NoBonus"},
-  {1, "EnlargeHintQueue"},
-  {2, "PieceTheft"},
-  {3, "Heal"},
-  {4, "SlowDown"},
-  {5, "ClearField"},
-  {6, "FlippedScreen"},
-  {7, "RotatingScreen"},
-  {8, "Wave"},
-  {9, "Lantern"},
-  {10, "CrazyPieces"},
-  {11, "TruncatedBlocks"},
-  {12, "NoHint"},
-  {13, "SpeedUp"}
-};
-
 const std::string kTextureWallPath = "resources/textures/wall/";
 
 const int kBonusesTextureSlot = 1;
@@ -35,26 +23,26 @@ const int kAimTextureSlot = 3;
 }
 
 std::unique_ptr<TextureArray> TextureFactory::createBonusesTexture() {
-    std::vector<sf::Image> images;
-    for (auto& textureName : kTextureBonusesNames) {
-        auto fullFileName = kTextureBonusesPath + textureName.second + ".png";
-        images.push_back( {});
-        assert(images.back().loadFromFile(fullFileName));
-    }
-    return std::make_unique<TextureArray>(kBonusesTextureSlot, images);
+  std::vector<sf::Image> images;
+  for (auto bonus : engine::ForEachBonus()) {
+    auto fullFileName = kTextureBonusesPath + getBonusName(bonus) + ".png";
+    images.push_back( {});
+    assert(images.back().loadFromFile(fullFileName));
+  }
+  return std::make_unique<TextureArray>(kBonusesTextureSlot, images);
 }
 
 std::unique_ptr<TextureArray> TextureFactory::createWallTexture() {
-    std::vector<sf::Image> images;
-    for (int i = 1;; ++i) {
-        auto fullFileName = kTextureWallPath + std::to_string(i) + ".jpg";
-        sf::Image img;
-        if (img.loadFromFile(fullFileName))
-          images.push_back(img);
-        else
-          break;
-    }
-    return std::make_unique<TextureArray>(kBackWallTextureSlot, images);
+  std::vector<sf::Image> images;
+  for (int i = 1;; ++i) {
+    auto fullFileName = kTextureWallPath + std::to_string(i) + ".jpg";
+    sf::Image img;
+    if (img.loadFromFile(fullFileName))
+      images.push_back(img);
+    else
+      break;
+  }
+  return std::make_unique<TextureArray>(kBackWallTextureSlot, images);
 }
 
 }

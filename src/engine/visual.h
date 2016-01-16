4c8d54a3560d158a42f7b22a61da30a316d8a3e3
Andrei Matveiakin <a.matveyakin@gmail.com>
2016-01-16 14:31:24 +0100

    Rename visual/misc to block_image

--- src/engine/visual.h
+++ src/engine/visual.h
@@ -1,7 +1,7 @@
 #pragma once
 
+#include "engine/visual/block_image.h"
 #include "engine/visual/effect.h"
-#include "engine/visual/misc.h"
 #include "engine/visual/motion.h"
 #include "engine/visual/object.h"
 #include "engine/visual/player_effect.h"
